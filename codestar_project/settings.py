af6aa18c2593bca3266e57ba455ed5cba2d2ae4b
flashdrag <flashdrag@gmail.com>
2023-04-25 20:49:21 +0100

    Fix `ALLOWED_HOSTS`

--- codestar_project/settings.py
+++ codestar_project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = ['https://django--blog.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['django--blog.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
