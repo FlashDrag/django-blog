e25cfc7850e6d3fd568ddfc292edc5073b6bb2f5
flashdrag <flashdrag@gmail.com>
2023-04-26 17:22:41 +0100

    Add base template and favicon

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('url_path/', views.<name of view function>, name='<name of view function>'),
+    path('', views.hello, name='hello'),
 ]
