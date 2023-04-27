7febe4b4cc96b1541aa65108318c2a553abf19b4
flashdrag <flashdrag@gmail.com>
2023-04-27 22:11:23 +0100

    Add summernote editor to admin panel for content field

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
 from .models import Post
+from django_summernote.admin import SummernoteModelAdmin
 
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    '''
+    Adds the summernote editor to the admin page for posts.
+
+    Decorator registers the both Post model and PostAdmin class
+    with our admin site, insead of registering it
+    separately with admin.site.register()
+    '''
+
+    # summernote_fields with the name of the field
+    # that we want to use the editor for
+    summernote_fields = ('content')
