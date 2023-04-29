1a96733f750951ad41c399ee5c1ad3c2ab9610ac
flashdrag <flashdrag@gmail.com>
2023-04-29 22:13:56 +0100

    Add frontend for base and index page templates

--- blog/views.py
+++ blog/views.py
@@ -9,7 +9,7 @@ class PostList(generic.ListView):
     # in descending order by created_on field
     # (using ordering option in model Meta class)
     queryset = Post.objects.filter(status=1)
-    template_name = 'post_list.html'
+    template_name = 'index.html'
     # Limit the number of posts per page to 6.
     # So, if the number of posts is greater than 6,
     # then django will automatically create pagination links
