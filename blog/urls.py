ffc76de33a2fce624c3bee484ed24cd73b88e79c
flashdrag <flashdrag@gmail.com>
2023-05-01 12:08:34 +0100

    Add post detail page; close #2, close #4

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
+    # 1st slug - path converter, that converts the slug to a string
+    # 2nd slug - keyword name, matches the 'slug' parameter in the view
+    # https://docs.djangoproject.com/en/3.2/topics/http/urls/#path-converters
+    path('post/<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Post
 
 
@@ -14,3 +14,30 @@ class PostList(generic.ListView):
     # So, if the number of posts is greater than 6,
     # then django will automatically create pagination links
     paginate_by = 6
+
+
+class PostDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        '''Retrieve a post by slug'''
+        # get only published posts(STATUS=Published)
+        queryset = Post.objects.filter(status=1)
+        # shortcut to get the post by slug or return 404
+        # if the post is not found
+        post = get_object_or_404(queryset, slug=slug)
+        # get approved comments for this post ordered by ascending date
+        comments = post.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        # if the user has liked the post, then set liked to True
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "liked": liked
+            },
+        )
