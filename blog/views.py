c1fe1d88b035bbe48f95b561c061668db17580c9
flashdrag <flashdrag@gmail.com>
2023-05-02 20:37:30 +0100

    Fix duplicate comment form submission with PRG pattern.
    The Post/Redirect/Get pattern involves redirecting the user to a new URL with a GET request instead of a POST request.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
+from django.http import HttpResponse
+from django.contrib import messages
 from .models import Post
 from .forms import CommentForm
 
@@ -17,6 +19,10 @@ class PostList(generic.ListView):
     paginate_by = 6
 
 
+# Post/Redirect/Get pattern
+# PRG pattern used to prevent duplicate form submissions
+# It means that after a POST request, we redirect to a GET request to the same
+# page with messages and errors if any.
 class PostDetail(View):
 
     def get(self, request, slug, *args, **kwargs):
@@ -39,7 +45,6 @@ class PostDetail(View):
             {
                 "post": post,
                 "comments": comments,
-                "commented": False,
                 "liked": liked,
                 "comment_form": CommentForm(),
             },
@@ -49,10 +54,6 @@ class PostDetail(View):
         '''Handle POST request to create a new comment'''
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
-        comments = post.comments.filter(approved=True).order_by('created_on')
-        liked = False
-        if post.likes.filter(id=self.request.user.id).exists():
-            liked = True
 
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
@@ -70,18 +71,13 @@ class PostDetail(View):
             # save the comment to the database
             new_comment.save()
 
+            messages.success(request, 'Thank you for your comment!\n'
+                             'Your comment is awaiting moderation.')
         else:
-            # if the form is invalid, return an empty form instance
-            comment_form = CommentForm()
+            messages.danger(request, 'Error adding your comment')
 
-        return render(
-            request,
-            "post_detail.html",
-            {
-                "post": post,
-                "comments": comments,
-                "commented": True,
-                "liked": liked,
-                "comment_form": comment_form,
-            },
-        )
+        # return redirect('post_detail', slug=post.slug)
+
+        # redirect to the same page keeping the same scroll position
+        # Note: keep in mind that history.back() does not 'reload' the page
+        return HttpResponse('<script>history.back();</script>')
